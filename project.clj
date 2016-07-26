91d73539b19cd8d6f01704957384030fd70ffeb1
Mike Sperber <sperber@deinprogramm.de>
2016-07-26 15:06:54 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/active-group/lawrence"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [active-clojure "0.3.0" :exclusions [org.clojure/clojure]]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.8"]]}})
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [active-clojure "0.12.0" :exclusions [org.clojure/clojure]]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.9"]]}})
